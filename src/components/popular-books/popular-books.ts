56bc0ffcd223656a4520b8d44a79a3e0e42a1622
Lucas Sena <lucassena.rj@gmail.com>
2018-03-05 19:03:45 -0300

    Add pupular books

---
+++ src/components/popular-books/popular-books.ts
@@ -0,0 +1,41 @@
+import { Component, ViewChild } from '@angular/core';
+import { BooksService } from '../../providers/books.service';
+import { Slides, NavController } from 'ionic-angular';
+import { BookDetailsPage } from '../../pages/book-details/book-details';
+
+@Component({
+  selector: 'popular-books',
+  templateUrl: 'popular-books.html',
+  providers: [
+    BooksService
+  ]
+})
+export class PopularBooksComponent {
+  @ViewChild(Slides) slides: Slides;
+
+  listBooks = new Array<any>();
+
+  constructor(private books: BooksService, private navCtrl: NavController) {
+    this.books.getBooks()
+      .subscribe(data=>{
+        const response = (data as any);
+        const object_return = JSON.parse(response._body);
+        this.listBooks = object_return.items;
+      }, error=>{
+        console.log(error);
+      })
+    
+  }
+
+  gotoNextSlide() {
+    this.slides.slideNext();
+  }
+
+  gotoPrevSlide() {
+    this.slides.slidePrev();
+  }
+
+  bookDetails(bookId){
+    this.navCtrl.push(BookDetailsPage, {id: bookId});
+  }
+}
